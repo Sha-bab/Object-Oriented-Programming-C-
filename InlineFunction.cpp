095b0b9f8667a22442e794900ea20dfbbf6b2842
Muhammad Shabab Sayem <sayemshabab@gmail.com>
2025-04-04 01:56:32 +0600

    Update InlineFunction.cpp

--- InlineFunction.cpp
+++ InlineFunction.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// Inline function to calculate the product of two numbers
+inline int product(int a, int b) {
+    return a * b;
+}
+
+// Function to calculate the received amount with a default interest rate
+float moneyReceived(int currentMoney, float rate = 1.06) {
+    return currentMoney * rate;
+}
+
+int main() {
+    int money;
+    cout << "Enter the amount of money you have: ";
+    cin >> money;
+
+    cout << "With a standard rate, your total amount will be: " << moneyReceived(money) << " tk" << endl;
+    cout << "For VIP members, your total amount will be: " << moneyReceived(money, 1.08) << " tk" << endl;
+
+    return 0;
+}
 
